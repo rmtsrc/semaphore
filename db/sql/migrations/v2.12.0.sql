8b729366b8057023a66f7cc47e2f345604ef300c
Denis Gukov <denguk@gmail.com>
2025-01-04 16:29:12 +0500

    fix(be): totp migration

--- db/sql/migrations/v2.12.0.sql
+++ db/sql/migrations/v2.12.0.sql
@@ -1,7 +1,7 @@
 create table user__totp(
   `id` integer primary key autoincrement,
   `user_id` int NOT NULL,
-  `secret` varchar(200) NOT NULL,
+  `url` varchar(250) NOT NULL,
   `created` datetime NOT NULL,
   unique (`user_id`),
   foreign key (`user_id`) references `user`(`id`) on delete cascade
