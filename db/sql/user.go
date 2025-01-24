eb148fb92b9fd827c807a7dd3352a24df77bc68e
Denis Gukov <denguk@gmail.com>
2025-01-25 00:32:14 +0500

    feat(users): store pro flag in sql

--- db/sql/user.go
+++ db/sql/user.go
@@ -70,22 +70,24 @@ func (d *SqlDb) UpdateUser(user db.UserWithPwd) error {
 			return err
 		}
 		_, err = d.exec(
-			"update `user` set name=?, username=?, email=?, alert=?, admin=?, password=? where id=?",
+			"update `user` set name=?, username=?, email=?, alert=?, admin=?, pro=?, password=? where id=?",
 			user.Name,
 			user.Username,
 			user.Email,
 			user.Alert,
 			user.Admin,
+			user.Pro,
 			string(pwdHash),
 			user.ID)
 	} else {
 		_, err = d.exec(
-			"update `user` set name=?, username=?, email=?, alert=?, admin=? where id=?",
+			"update `user` set name=?, username=?, email=?, alert=?, admin=?, pro=? where id=?",
 			user.Name,
 			user.Username,
 			user.Email,
 			user.Alert,
 			user.Admin,
+			user.Pro,
 			user.ID)
 	}
 
